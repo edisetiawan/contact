4c6f33b51fbb05e1c324de4423074bee970300ba
edisetiawan <edi2992@gmail.com>
2016-01-31 16:12:32 +0700

    error

---
+++ 404.php
@@ -0,0 +1,341 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head profile="http://gmpg.org/xfn/11"> 
+<!-- title --> 
+<title>OLX.co.id - Error</title> 
+<!-- site icons --> 
+<link rel="shortcut icon" href="/olxid/img/favicon.ico" type="image/x-icon" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="robots" content="noindex, nofollow" /> 
+<meta http-equiv="expires" content="-1"> 
+<meta http-equiv="pragma" content="no-cache"> 
+<meta http-equiv="cache-control" content="no-cache"> 
+<style type="text/css"> 
+html,body {
+	height: 100%;
+}
+ 
+html,body,h1,h2,h3,h4,h5,h6,div,span,blockquote,p,address,form,fieldset,img,ul,ol,dl,dt,dd,li,hr,table,td,th,strong,em,sup,sub,dfn,ins,del,q,cite,var,samp,code,kbd,tt
+	{
+	margin: 0;
+	padding: 0;
+	border: 0;
+}
+ 
+img,fieldset {
+	border: none;
+}
+ 
+a,a:visited {
+	text-decoration: none;
+}
+ 
+li {
+	list-style: none;
+}
+ 
+a img {
+	border: 0em;
+	vertical-align: middle;
+}
+ 
+label {
+	cursor: pointer;
+}
+ 
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+ 
+th,td {
+	text-align: left;
+}
+ 
+address {
+	display: block;
+	font-style: normal;
+}
+ 
+blockquote,q {
+	quotes: none;
+}
+ 
+div.clear {
+	clear: both;
+	overflow: hidden;
+	height: 0px;
+}
+ 
+body {
+	color: #2b2b2b;
+	text-align: center;
+	font-size: 100%;
+	line-height: 1em;
+	background: #EEEEEE;
+}
+ 
+body,input,textarea,select,option,optgroup,button {
+	font-family: "Verdana", Arial, Helvetica, sans-serif;
+}
+ 
+.wrapper {
+	text-align: left;
+	margin: 0 auto;
+	font-size: 12px;
+	width: 898px;
+}
+ 
+#header-container .wrapper {
+	height: 82px;
+	position: relative;
+}
+ 
+#body-container .wrapper h3 {
+	line-height: 39px;
+	margin-bottom: 42px;
+	border-bottom: 1px solid #D3D2CA;
+	padding-bottom: 18px;
+}
+ 
+#body-container .wrapper h3 a {
+	display: block;
+	width:84px;
+	height:84px;
+	background: url(/olxid/img/logo.png) no-repeat left top;
+	text-indent: -1000em;
+}
+ 
+#body-container .wrapper h1 {
+	font-size: 32px;
+	line-height: 39px;
+	background: url(/olxid/img/icon-big-alert.png)
+		no-repeat left top;
+	padding-left: 100px;
+	min-height: 79px;
+	margin: 42px 0px 30px 0px;
+}
+ 
+#body-container .wrapper h1 em {
+	font-style: normal;
+	display: block;
+	font-size: 25px;
+	font-weight: normal;
+}
+ 
+#body-container .wrapper .layer-rounded {
+	padding-top: 27px;
+	zoom: 1;
+}
+ 
+#body-container .wrapper .layer-rounded .top,#body-container .wrapper .layer-rounded .bottom
+	{
+	height: 11px;
+	overflow: hidden;
+}
+ 
+#body-container .wrapper .layer-rounded .top {
+	background: url(/olxid/img/shadow_top.png) no-repeat
+		left top;
+}
+ 
+#body-container .wrapper .layer-rounded .bottom {
+	background: url(/olxid/img/shadow_bottom.png)
+		no-repeat left top;
+}
+ 
+#body-container .wrapper .layer-rounded .inner {
+	background: url(/olxid/img/shadow.png) repeat-y left
+		top;
+}
+ 
+#body-container .wrapper .layer-rounded .inner .inner-2nd {
+	margin: 0px 11px;
+	padding: 20px 30px 30px;
+	background: #ffffff;
+}
+ 
+#body-container .wrapper ul.links {
+	color: #626262;
+	overflow: hidden;
+}
+ 
+#body-container .wrapper ul.links li {
+	float: left;
+	margin: 0px 5px 0px 5px;
+	line-height: 30px;
+}
+ 
+#body-container .wrapper ul.links li span.slash {
+	margin-right: 10px;
+	color: #d9dee2;
+}
+ 
+#body-container .wrapper .layer-rounded-gray {
+	margin-top: 30px;
+	background: #f3f3f3;
+}
+ 
+#body-container .wrapper .layer-rounded-gray {
+	padding: 10px;
+}
+ 
+#body-container .wrapper a.button,#body-container .wrapper a.button span,#body-container .wrapper a.button span em
+	{
+	background-image: url(/olxid/img/core-buttons.png);
+	background-repeat: no-repeat;
+	color: #fff;
+}
+ 
+#body-container .wrapper a.button {
+	height: 30px;
+	line-height: 30px;
+	float: left;
+	font-size: 14px;
+	font-weight: bold;
+	background-position: 0px -105px;
+	color: #ffffff;
+}
+ 
+#body-container .wrapper a.button span,#body-container .wrapper a.button span em
+	{
+	float: left;
+	height: 30px;
+}
+ 
+#body-container .wrapper a.button span {
+	background-position: right -165px;
+}
+ 
+#body-container .wrapper a.button span em {
+	font-style: normal;
+	margin: 0px 5px 0px 5px;
+	padding: 0px 7px 0px 7px;
+	background-position: 0px -135px;
+	background-repeat: repeat-x;
+}
+ 
+#body-container .wrapper a.button-refresh span em {
+	margin-left: 28px;
+}
+ 
+a {
+	outline: none;
+	color: #3677B6;
+	text-decoration: none;
+}
+ 
+a:link {
+	
+} /* unvisited link */
+a:hover {
+	text-decoration: underline;
+} /* mouse over link */
+a:visited {
+	
+} /* visited link */
+a:active {
+	
+} /* selected link */
+a.link {
+	color: #b2d0f4;
+	text-decoration: underline;
+}
+ 
+a.link span {
+	color: #3677B6;
+}
+</style> 
+<!--[if IE 6]>
+    	<style type="text/css">
+    		#body-container .wrapper .layer-rounded .top,
+    		#body-container .wrapper .layer-rounded .bottom,
+    		#body-container .wrapper .layer-rounded .inner,
+    		#body-container .wrapper h3 a{
+    			background:none !important;
+    		}
+    		#body-container .wrapper .layer-rounded .inner,
+			#body-container .wrapper .layer-rounded-gray{
+    			zoom:1;
+    		}
+    		#body-container .wrapper .layer-rounded .top{
+    			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="/olxid/img/shadow_top.png", sizingMethod="crop") !important;
+    		}
+    		#body-container .wrapper .layer-rounded .bottom{
+    			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="/olxid/img/shadow_bottom.png", sizingMethod="crop") !important;
+    		}
+    		#body-container .wrapper .layer-rounded .inner{
+    			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="/olxid/img/shadow.png", sizingMethod="scale") !important;
+    		}
+    		#body-container .wrapper h3 a{
+    			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="/olxid/img/logo.png", sizingMethod="crop") !important;}
+    		}
+    	</style>
+    <![endif]--> 
+</head> 
+<body xml:lang="en"> 
+	<div id="body-container"> 
+		<div class="wrapper"> 
+			<div class="layer-rounded"> 
+				<div class="top"></div> 
+				<div class="inner"> 
+					<div class="inner-2nd"> 
+						<h3> 
+							<a href="http://olx.co.id" title="OLX.co.id">OLX.co.id</a> 
+						</h3> 
+						<h1> 
+							Hello Salam Kenal Gan <em>Kami Developer Code Indonesia</em> 
+						</h1> 
+						<p>sebelumnya terimakasih telah berkunjung di website ini</p>
+						<p><a href="http://develop-code.com">http://develop-code.com</a> adalah tempat demo aplikasi berbasis web yang dibuat oleh Edi Setihawan dan dibiayai oleh sang investor Gufron Edi Wibowo. </p> 
+						<div class="layer-rounded-gray"> 
+							<ul class="links"> 
+								<!--<li> 
+									<a href="" title="Try again" class="button button-refresh"> 
+										<span> 
+											<em>Coba lagi </em> 
+										</span> 
+									</a> 
+								</li>  -->
+								
+								<li>untuk informasi lebih lanjut anda bisa hubungi saya <a href="http://edisetihawan.com/">disini</a> </li> 
+								<!--<li> 
+									<a href="http://olx.co.id" title="Go to main page" class="link"> 
+										<span>Ke halaman depan </span> 
+									</a> 
+								</li> 
+								<li> 
+									<span class="slash">|</span> 
+									<a href="/contact" title="Contact Us" class="link"> 
+										<span>Hubungi kami </span> 
+									</a> 
+								</li> 
+								<li> 
+									<span class="slash">|</span> 
+									<a href="/help" title="Help" class="link"> 
+										<span>Bantuan </span> 
+									</a> 
+								</li>  -->
+							</ul> 
+						</div> 
+					</div> 
+				</div> 
+				<div class="bottom"></div> 
+			</div> 
+		</div> 
+	</div> 
+	<!-- APPERRORMSG --> 
+<script> 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 
+  ga('create', 'UA-57804872-1', 'auto');
+  ga('send', 'pageview');
+ 
+</script> 
+</body> 
+</html>
